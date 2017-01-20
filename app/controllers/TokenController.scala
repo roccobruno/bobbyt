452219c89484d31279b462a3cf91830891d181b8
Rocco Bruno <rocco.bruno@digital.hmrc.gov.uk>
2017-01-20 17:24:31 +0000

    clean up unused code

--- app/controllers/TokenController.scala
+++ app/controllers/TokenController.scala
@@ -13,27 +13,6 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 import scala.concurrent.Future
 
-class TokenController extends Controller {
-
-
-  def callback() = Action.async { implicit request =>
-
-    val token = request.headers.get(HeaderNames.AUTHORIZATION)
-
-    Logger.info(s"token - $token")
-
-     token.fold(Future.successful(Unauthorized)){
-       value =>
-         Auth0Config.decodeAndVerifyToken(value.split("Bearer")(1)) match {
-           case Right(token) => Future.successful(Ok)
-           case Left(message) => Logger.info(message); Future.successful(Unauthorized)
-         }
-     }
-  }
-
-
-}
-
 
 trait TokenChecker {
   val repository: BobbytRepository
